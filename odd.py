b24706d43867179f808971ae03ea94bf929e097e
RevathiRathi <46440882+RevathiRathi@users.noreply.github.com>
2019-01-07 10:06:08 +0000

    Update odd.py

--- odd.py
+++ odd.py
@@ -1,8 +1,8 @@
 n=int(input())
 if n<0:
-	print("Invalid")
+    print("Invalid")
 else:
-	if (n%2)==0:
-		print("Even")
+    if (n%2)==0:
+	print("Even")
     elif (n%2)!=0:
-    	print("Odd")
+	print("Odd")
