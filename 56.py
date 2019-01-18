bf6115ff3af21f268dd28fe0f1b308c2caf5a5a1
RevathiRathi <46440882+RevathiRathi@users.noreply.github.com>
2019-01-18 10:05:09 +0530

    Update 56.py

--- 56.py
+++ 56.py
@@ -1,5 +1,12 @@
 n=input()
-if n.isalpha() and n.isdigit():
-	print("Yes")
-else:
+a,d=0,0
+for i in n:
+	if i.isalpha():
+		a=1
+	elif i.isdigit():
+		d=1
+	if a==1 and d==1:
+		print("Yes")
+		break
+if a==0 and d==0:
 	print("No")
