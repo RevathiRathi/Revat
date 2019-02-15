0cefda3a0dec73ffd321fb75e197e28e9e6d776d
RevathiRathi <46440882+RevathiRathi@users.noreply.github.com>
2019-02-15 15:47:06 +0530

    Update 111.py

--- 111.py
+++ 111.py
@@ -1,11 +1,9 @@
 n,k=map(int,input().split())
 c=0
-for i in range(n,k):
-	i1=2
-	while i1<n:
-		if n%i1==0:
+for i in range(n,k+1):
+	for j in range(2,i+1):
+		if i%j==0:
 			break
-		i1+=1
-	if n==i1:
-		c+=1
+	if j==i:
+		c=c+1
 print(c)
