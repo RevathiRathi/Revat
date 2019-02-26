a3b5e10fe05cee58e9a369cb6fc6db52e6565fa5
RevathiRathi <46440882+RevathiRathi@users.noreply.github.com>
2019-02-26 23:20:52 +0530

    Update er.py

--- er.py
+++ er.py
@@ -2,11 +2,11 @@
 n,k=map(int,input().split())
 p=[]
 s=""
-l=list(map(int,input().split()))
+l1=list(map(int,input().split()))
 for i in range(0,k):
-	l.append(l[0])
-	l.remove(l[0])
-for i in l:
+	l1.append(l1[0])
+	l1.remove(l1[0])
+for i in l1:
    
         s=s+str(i)+" "
 print(s.strip())
