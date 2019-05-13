253d27e908fad989bd6604b58dea1e6cc95d328d
RevathiRathi <46440882+RevathiRathi@users.noreply.github.com>
2019-05-13 21:48:03 +0530

    Create hunt10.py

---
+++ hunt10.py
@@ -0,0 +1,11 @@
+n,k=map(int,input().split(" "))
+a=[int(x) for x in input().split()]
+b=[int(y) for y in input().split()]
+c=0
+for i in range(0,len(b)):
+	if b[i] in a:
+		c+=1
+if len(b)==c:
+	print("yes")
+else:
+	print("no")
