07944b9d98bed814239a274ad9cc3ac89513ed3f
RevathiRathi <46440882+RevathiRathi@users.noreply.github.com>
2019-05-13 15:36:29 +0530

    Update hunt6.py

--- hunt6.py
+++ hunt6.py
@@ -6,7 +6,8 @@ for i in range(len(l)):
 	for j in range(i+1,len(l)):
 		if l[i]==l[j]:
 			a.append(l[i])
-			c=c+1
-print(a[0])
+for i in range(0,len(a)-1):
+	c=c+1
+	print(a[0])
 if c==0:
 	print("unique")
