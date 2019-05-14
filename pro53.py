b4790d3580d02a382ef910fb14af3ed866640808
RevathiRathi <46440882+RevathiRathi@users.noreply.github.com>
2019-05-14 21:48:37 +0530

    Update pro53.py

--- pro53.py
+++ pro53.py
@@ -1,8 +1,9 @@
-n=input()
-s=n.lower()
+#rrrr
+s=input()
+n=s.lower()
 k=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 
-if all(i in s for i in k):
+if all(i in n for i in k):
     print("yes")
 else:
     print("no")
