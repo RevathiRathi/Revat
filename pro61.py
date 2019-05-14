b1614daf6fcd0b6be36a3654503a8f5b8ca52edd
RevathiRathi <46440882+RevathiRathi@users.noreply.github.com>
2019-05-14 16:57:56 +0530

    Create pro61.py

---
+++ pro61.py
@@ -0,0 +1,11 @@
+s=input()
+s1=input()
+l=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+l1=[]
+for i,j in zip(s,s1):
+	l1.append((l.index(i)+1)+(l.index(j)+1))
+for i in range(0,len(l1)):
+	if l1[i]>26:
+		l1[i]=l1[i]%26
+for i in l1:
+	print(l[i-1],end="")
