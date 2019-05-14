63dfe5cf62f17a8cfb7942eed0d5c5e1c5d6cae5
RevathiRathi <46440882+RevathiRathi@users.noreply.github.com>
2019-05-14 12:15:17 +0530

    Update pro2.py

--- pro2.py
+++ pro2.py
@@ -1,8 +1,6 @@
-n,k=map(int,input().split(" "))
-n=str(n)
-l=[]
-s=len(n)-k
-for i in range(0,(len(n)-s)+1):
-	d=n[i:i+s]
-	l.append(d)
-print(min(l))
+from itertools import combinations
+n,k=map(int,input().split())
+r=len(str(n))
+l=list(combinations(str(n),r-k))
+l=sorted(l)
+print("".join(l[0]))
