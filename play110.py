2db2833404cbb6cdb152d5052c55f9074dc48945
RevathiRathi <46440882+RevathiRathi@users.noreply.github.com>
2019-05-20 16:35:29 +0530

    Create play110.py

---
+++ play110.py
@@ -0,0 +1,12 @@
+#reva
+n=int(input())
+l=list(map(int,input().split()))
+a=[]
+c=[]
+for i in range(1):
+	for j in range(i,n):
+		a.append(sum(l[:j+1]))
+b=a[::-1]
+for i in range(n):
+	c.append(str(a[i]+b[i]))
+print(" ".join(c))
