70e0f0008567bc72b5c02aecf92e240b766dfd16
RevathiRathi <46440882+RevathiRathi@users.noreply.github.com>
2019-05-22 12:15:48 +0530

    Update pro24.py

--- pro24.py
+++ pro24.py
@@ -1,4 +1,3 @@
-#reva
 n=int(input())
 a=[]
 k=bin(2**n-1)[2::]
@@ -7,7 +6,8 @@ for i in range(0,2**n):
 	s=bin(i)[2::]
 	if len(s)<l:
 		a.append([s.count("1"),(l-len(s))*"0"+s])
-		print(a)
 	else:
 		a.append([s.count("1"),s])
-print(a)
+a.sort()
+for i in range(0,len(a)):
+	print(a[i][1])
