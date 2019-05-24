f992f4cbe06677773f6a11fb813e55ac9bf03e3e
RevathiRathi <46440882+RevathiRathi@users.noreply.github.com>
2019-05-24 19:27:01 +0530

    Create play145.py

---
+++ play145.py
@@ -0,0 +1,10 @@
+#reva
+n=int(input())
+l=[int(x) for x in input().split()]
+for i in range(1,len(l)):
+    if l[i]-l[i-1]==1:
+        print("yes")
+        break
+    else:
+        print("no")
+        break
