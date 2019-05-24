81ce4d1aa9ac0acdaab386e36aa5d25550333742
RevathiRathi <46440882+RevathiRathi@users.noreply.github.com>
2019-05-24 15:03:23 +0530

    Create play133.py

---
+++ play133.py
@@ -0,0 +1,13 @@
+#rev
+n,k=map(int,input().split())
+l=[]
+c=0
+for i in range(0,n):
+    l.append(list(map(int,input().split())))
+for i in range(0,len(l)):
+    if l[i][1]==k:
+        c+=1
+if c==0:
+    print("no")
+else:
+    print("yes")
