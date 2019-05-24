a635153fb56202d45772ff7134a13db881864d98
RevathiRathi <46440882+RevathiRathi@users.noreply.github.com>
2019-05-24 15:29:49 +0530

    Update play136.py

--- play136.py
+++ play136.py
@@ -1,9 +1,7 @@
 #rr
 n,k=map(int,input().split())
 l=[int(x) for x in input().split()]
-for i in l:
-    if i==k:
-        print("yes",l.count(k))
-        break
-    else:
-        print("no")
+if k in l:
+    print("yes",l.count(k))
+else:
+    print("no")
