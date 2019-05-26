98e0ab6c7199c8c20fd2b411d2afdc3e83e25074
RevathiRathi <46440882+RevathiRathi@users.noreply.github.com>
2019-05-26 12:06:42 +0530

    Update play164.py

--- play164.py
+++ play164.py
@@ -1,5 +1,11 @@
 #rr
 n,k=map(int,input().split())
 l=[int(x) for x in input().split()]
+m=0
 if k in l:
     print(k)
+else:
+    for i in l:
+        if i>m and i<k:
+            m=i
+    print(m)
