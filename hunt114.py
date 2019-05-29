e926456689eea6ca7977fad594effa757ec1b3bf
RevathiRathi <46440882+RevathiRathi@users.noreply.github.com>
2019-05-29 19:41:29 +0530

    Create hunt114.py

---
+++ hunt114.py
@@ -0,0 +1,16 @@
+#r
+n,m=map(int,input().split())
+k=0
+for i in range(n,m):
+    a=str(i)
+    s=0
+    for i in range(len(a)):
+        s=s+int(a[i])
+        
+    c=0
+    for i in range(1,s):
+        if s%i==0:
+            c=c+1
+    if c==1:
+        k=k+1
+print(k)
