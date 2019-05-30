9aa8064aea00bf9667583691535616b3f68a281c
RevathiRathi <46440882+RevathiRathi@users.noreply.github.com>
2019-05-30 12:50:10 +0530

    Update hunt56.py

--- hunt56.py
+++ hunt56.py
@@ -4,7 +4,7 @@ l=[int(x) for x in input().split()]
 a=1000
 for i in range(0,len(l)-1):
     for j in range(i+1,len(l)):
-        if abs(l[i]+l[i+1])<a:
-            a=abs(l[i]+l[i+1])
-            r,g=l[i],l[i+1]
+        if abs(l[i]+l[j])<a:
+            a=abs(l[i]+l[j])
+            r,g=l[i],l[j]
 print(r,g)
