573d273ca80f455d01b219e7f0cd90ed1fdd202c
RevathiRathi <46440882+RevathiRathi@users.noreply.github.com>
2019-05-30 18:15:55 +0530

    Create hunt58.py

---
+++ hunt58.py
@@ -0,0 +1,9 @@
+#r
+n,k=map(int,input().split())
+l=[int(x) for x in input().split()]
+c=0
+for i in range(0,len(l)):
+    for j in range(i+1,len(l)):
+        if abs(l[i]-l[j])==k:
+            c+=1
+print(c)
