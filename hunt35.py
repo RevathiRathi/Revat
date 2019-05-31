7a9fb5a5590f839c70977004699665e8dc96ca50
RevathiRathi <46440882+RevathiRathi@users.noreply.github.com>
2019-05-31 19:00:03 +0530

    Update hunt35.py

--- hunt35.py
+++ hunt35.py
@@ -1,13 +1,13 @@
 #r
 s=input()
-c=0
+a=0
 for i in range(len(s)):
     if s[:i]==s[i+1:]:
-        c=0
+        a=0
         break
     else:
-        c=1
-if c==0:
+        a=1
+if a==0:
     print("YES")
 else:
     print("NO")
