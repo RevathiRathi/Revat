07d7ffa8f08bb05c9460fd319e675ce6f5adf96f
RevathiRathi <46440882+RevathiRathi@users.noreply.github.com>
2019-06-01 11:45:06 +0530

    Create hunt70.py

---
+++ hunt70.py
@@ -0,0 +1,13 @@
+#r
+from itertools import combinations
+n,k,t=map(int,input().split())
+l=list(map(int,input().split()))
+c=0
+x= list(combinations(l,k))
+for i in range(len(x)):
+	if sum(x[i])==t:
+		c=1
+		print("YES")
+		break
+if c==0:
+	print("NO")
