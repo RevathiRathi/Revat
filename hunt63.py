925db924dc9947006950a7c49f6efdc989b4e544
RevathiRathi <46440882+RevathiRathi@users.noreply.github.com>
2019-06-01 10:53:26 +0530

    Update hunt63.py

--- hunt63.py
+++ hunt63.py
@@ -1,10 +1,10 @@
 #r
 n=int(input())
-l=[int(x) for x in input().split()]
-l1=[]
-for i in range(len(l)-1):
-    l2=l[i+1::]
-    m=max(l2)
-    l1.append(m)
-l1.append(0)
+l1=[int(x) for x in input().split()]
+l2=[]
+for i in range(len(l1)-1):
+	l=l1[i+1::]
+	m=max(l)
+	l2.append(m)
+l2.append(0)
 print(*l2)
