efc73dd71a4949a08013a953a07c156d88e68759
RevathiRathi <46440882+RevathiRathi@users.noreply.github.com>
2019-06-02 13:40:57 +0530

    Update hunt74.py

--- hunt74.py
+++ hunt74.py
@@ -1,10 +1,10 @@
 #rrrrr
 s=input()
 s1=input()
-l=""
+a=""
 for i in range(len(s)):
     for j in range(len(s),-1,-1):
         if (s[i:j] in s1):
-            if(len(s[i:j])>=len(l)):
-                l=s[i:j]
-print(l)
+            if(len(s[i:j])>=len(a)):
+                a=s[i:j]
+print(a)
