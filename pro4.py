b11411cb5c27d719d4047478ba96458b4c4e22d5
RevathiRathi <46440882+RevathiRathi@users.noreply.github.com>
2019-06-03 19:19:10 +0530

    Update pro4.py

--- pro4.py
+++ pro4.py
@@ -1,12 +1,12 @@
 #rrrr
 s,t=map(str,input().split())
-l=0
+l1=0
 if len(s)>len(t):
   s,t=t,s
 i=0
 while i<len(s):
-  l+=(ord(t[i])-ord(s[i]))
+  l1+=(ord(t[i])-ord(s[i]))
   i+=1
 for i in range(i,len(t)):
-  l+=ord(t[i])-ord('a')+1
-print(l)
+  l1+=ord(t[i])-ord('a')+1
+print(l1)
