67a0e659353c6e59e98b1e67621fed06548cd67c
RevathiRathi <46440882+RevathiRathi@users.noreply.github.com>
2019-06-03 11:38:10 +0530

    Create hunt106.py

---
+++ hunt106.py
@@ -0,0 +1,12 @@
+#r
+s=input()
+l=[]
+if s==s[::-1]:
+	print(s)
+else:
+	for i in range(len(s)):
+		for j in range(len(s),-1,-1):
+			a=s[i:j+1]
+			if a==a[::-1]:
+				l.append(len(a))
+print(max(l))	
